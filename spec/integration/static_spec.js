9d830fda8635aa5a426a2a63e319a1e39b6dc4a2
Lauren Forcey <lauren4c@gmail.com>
2019-02-25 09:09:25 -0700

    Completed Intro to Express Assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,12 +1,27 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const marco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   describe("GET /", () => {
     it("should return status code 200", done => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+  });
+  describe("GET /marco", () => {
+    it("should return status code of 200", done => {
+      request.get(marco, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        done();
+      });
+    });
+    it("should return 'polo' in the body", done => {
+      request.get(marco, (err, res, body) => {
+        expect(res.body).toBe("polo");
         done();
       });
     });
