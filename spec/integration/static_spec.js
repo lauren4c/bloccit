18d44dbe356cdf152dfcb156faa98f03644052ab
Lauren Forcey <lauren4c@gmail.com>
2019-02-27 08:38:58 -0700

    Completed Routing, controller & views assignment

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const about = "http://localhost:3000/about";
 
 describe("routes : static", () => {
   describe("GET /", () => {
@@ -8,6 +9,14 @@ describe("routes : static", () => {
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
+        done();
+      });
+    });
+  });
+  describe("GET /about", () => {
+    it("Should have the string 'About Us' in the body", done => {
+      request.get(about, (err, res, body) => {
+        expect(body).toContain("About Us");
         done();
       });
     });
