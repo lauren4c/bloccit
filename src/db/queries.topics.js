5cee9822084cec278d200fc2d3bd22e5e8ba143d
Lauren Forcey <lauren4c@gmail.com>
2019-03-06 08:28:46 -0700

    Completed CP work for Post Resource Pt - CRUD.

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
   getAllTopics(callback) {
@@ -24,7 +25,14 @@ module.exports = {
       });
   },
   getTopic(id, callback) {
-    return Topic.findByPk(id)
+    return Topic.findByPk(id, {
+      include: [
+        {
+          model: Post,
+          as: "posts"
+        }
+      ]
+    })
       .then(topic => {
         callback(null, topic);
       })
