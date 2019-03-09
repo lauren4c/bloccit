a88916156f68e398048776b50536600c786f482f
Lauren Forcey <lauren4c@gmail.com>
2019-03-08 18:03:56 -0700

    Completed Assignment work for Topic Validation.

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -78,6 +78,29 @@ describe("routes : topics", () => {
         }
       );
     });
+    it("should not create a new topic that fails validations", done => {
+      const options = {
+        url: `${base}${this.topic.id}/create`,
+        form: {
+          //#1
+          title: "a",
+          body: "b"
+        }
+      };
+
+      request.post(options, (err, res, body) => {
+        //#2
+        Topic.findOne({ where: { title: "a" } })
+          .then(topic => {
+            expect(topic).toBeNull();
+            done();
+          })
+          .catch(err => {
+            console.log(err);
+            done();
+          });
+      });
+    });
   });
   describe("GET /topics/:id", () => {
     it("should render a view with the selected topic", done => {
